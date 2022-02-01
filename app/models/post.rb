922521f2d8ba011c8125250be6e14df2f2844167
Sarah Nosal <nosalsm@gmail.com>
2022-02-01 11:12:27 -0600

    completed assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+
+    validate :title_is_clickbait
+
+    CLICKBAIT_PATTERNS = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top \d/i,
+        /Guess/i
+    ]
+
+    def title_is_clickbait
+        if CLICKBAIT_PATTERNS.none?{|pat| pat.match title}
+            errors.add(:title, "Not clickbaity enough")
+        end
+    end
+
 end
